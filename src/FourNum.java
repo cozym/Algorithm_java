98e594396ec5f67febd5b021f82c5cd40d959c75
cozym <ddongyee99@daum.net>
2023-04-10 23:57:50 +0900

    Add completed FourNum

--- src/FourNum.java
+++ src/FourNum.java
@@ -1,7 +1,20 @@
 // 네 수 (10824)
 
-public class FourNum {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class FourNum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] sb = br.readLine().split(" ");
+        try {
+            long num = Long.parseLong(sb[0] + sb[1]) + Long.parseLong(sb[2] + sb[3]);
+            System.out.println(num);
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
     }
 }
