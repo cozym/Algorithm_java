650566abc2220edd0897476f993698b08bff9ddb
cozym <ddongyee99@daum.net>
2023-04-13 00:31:22 +0900

    Add Remainder

--- src/Remainder.java
+++ src/Remainder.java
@@ -1,7 +1,21 @@
 // 나머지 (10430)
 
-public class Remainder {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Remainder {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] abc = br.readLine().split(" ");
+
+        int a = Integer.parseInt(abc[0]);
+        int b = Integer.parseInt(abc[1]);
+        int c = Integer.parseInt(abc[2]);
+
+        System.out.println((a+b)%c);
+        System.out.println((a%c+b%c)%c);
+        System.out.println((a*b)%c);
+        System.out.println((a%c*b%c)%c);
     }
 }
