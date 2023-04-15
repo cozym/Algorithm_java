2e1b98c089fa922972825eef349a1bd0440650a0
cozym <ddongyee99@daum.net>
2023-04-15 23:40:18 +0900

    Incomplete FindPrimeNum

---
+++ src/FindPrimeNum.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 소수 찾기 (1978)
+public class FindPrimeNum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
