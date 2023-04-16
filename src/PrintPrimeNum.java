ad404a34aec50b6118cbc429a15d2f87b232125c
cozym <ddongyee99@daum.net>
2023-04-16 23:32:30 +0900

    Incomplete 1929

---
+++ src/PrintPrimeNum.java
@@ -0,0 +1,10 @@
+import java.io.IOError;
+import java.io.IOException;
+
+// 소수 구하기 (1929)
+public class PrintPrimeNum {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
