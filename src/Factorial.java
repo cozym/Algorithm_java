35c777efd3907df26e084841f687d5cd7f710596
cozym <ddongyee99@daum.net>
2023-04-19 23:11:48 +0900

    Incomplete FactorialZero

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // 팩토리얼 (10872)
 public class Factorial {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
---
+++ src/FactorialZero.java
@@ -0,0 +1,6 @@
+// 팩토리얼 0의 개수 (1676)
+public class FactorialZero {
+    public static void main(String[] args) {
+        
+    }
+}
