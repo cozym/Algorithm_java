370e1278740050f6c014bce25f24a5c9bcb8cfbe
cozym <ddongyee99@daum.net>
2023-12-02 23:10:58 +0900

    modified 1260 javaVersion

--- src/Boj1260.java
+++ src/Boj1260.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // DFS와 BFS
 public class Boj1260 {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
