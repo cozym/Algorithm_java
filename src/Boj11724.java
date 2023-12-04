3c189c3734118ebb66e00216d70ddfeb9002a70e
cozym <ddongyee99@daum.net>
2023-12-04 22:51:59 +0900

    modified 11724

--- src/Boj11724.java
+++ src/Boj11724.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Boj11724 {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
