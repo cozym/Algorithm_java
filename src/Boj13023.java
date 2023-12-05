b69ccfb9c726f38f0a005c613089a2034848af79
cozym <ddongyee99@daum.net>
2023-12-05 21:50:58 +0900

    succeed 13023 javaVersion

--- src/Boj13023.java
+++ src/Boj13023.java
@@ -1,33 +1,53 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Boj13023 {
-    private static List<ArrayList> relationship;
+    private static ArrayList<Integer>[] relationship;
+    private static boolean[] visit;
+    private static int result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        relationship = new ArrayList<>();
+        visit = new boolean[N];
+        result = 0;
+        relationship = new ArrayList[N];
         for (int i = 0; i < N; i++) {
-            relationship.add(new ArrayList<Integer>());
+            relationship[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            relationship.get(a).add(b);
-            relationship.get(b).add(a);
+            relationship[a].add(b);
+            relationship[b].add(a);
         }
-        System.out.println(relationship);
+
+        for (int i = 0; i < N; i++) {
+            if (result==0) {
+                dfs(i, 0);
+            }
+        }
+        System.out.println(result);
+    }
+
+    static void dfs(int idx, int depth) {
+        if (depth==4) {
+            result = 1;
+            return; // 스택 탈출
+        }
+        visit[idx] = true;
+        for (int i : relationship[idx]) {
+            if (!visit[i]) {
+                dfs(i, depth + 1);
+            }
+        }
+        visit[idx] = false;
     }
 }
