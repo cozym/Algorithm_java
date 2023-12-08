77bd875f62d0b0b649fe5324917bf758c01a0e23
cozym <ddongyee99@daum.net>
2023-12-08 23:04:04 +0900

    modified 2667

--- src/Boj2667.java
+++ src/Boj2667.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Boj2667 {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
