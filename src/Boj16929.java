8dd1b4ede3ff76b51f43d82f01eadb345945d02b
cozym <ddongyee99@daum.net>
2024-01-11 20:17:58 +0900

    feat: succeed 16929 javaVersion

--- src/Boj16929.java
+++ src/Boj16929.java
@@ -1,5 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // Two Dots
 public class Boj16929 {
-  public static void main(String[] args){
+  static int N,M,depth;
+  static String res;
+  static char[][] pad;
+  static boolean[][] visit;
+  static int[] moveX = {-1,0,0,1};
+  static int[] moveY = {0,-1,1,0};
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    pad = new char[N][M];
+    res = "No";
+
+    for (int i = 0; i < N; i++) {
+      String line = br.readLine();
+      for (int j = 0; j < M; j++) {
+        pad[i][j] = line.charAt(j);
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        visit = new boolean[N][M];
+        visit[i][j] = true;
+        searchCycleDfs(new int[]{i,j},i,j);
+      }
+    }
+    System.out.println(res);
+  }
+
+  static void searchCycleDfs(int[] start,int y, int x) {
+    int roadBlock = 0;
+    for (int i = 0; i < 4; i++) {
+      int nextX = x + moveX[i];
+      int nextY = y + moveY[i];
+      if (start[0] == nextY && start[1] == nextX && depth >= 3) {
+        res = "Yes";
+        return;
+      }
+      if (0 <= nextX && nextX < M && 0 <= nextY && nextY < N && !visit[nextY][nextX] && pad[y][x] == pad[nextY][nextX]) {
+        visit[y][x] = true;
+        depth++;
+        searchCycleDfs(start,nextY,nextX);
+      } else {
+        roadBlock++;
+      }
+      if (roadBlock==4) {
+        depth = 0;
+      }
+    }
   }
 }
