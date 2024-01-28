affddf635bb90959f92040891d0508fcc0410ca9
cozym <ddongyee99@daum.net>
2024-01-28 22:53:20 +0900

    refactor: modified 1037

--- src/Boj1037.java
+++ src/Boj1037.java
@@ -1,7 +1,9 @@
+import java.io.BufferedReader;
+
 // 약수
 public class Boj1037 {
     static int N;
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader()
     }
 }
