f84ab3b6e483023b05b67745e8c6d4e0414b124c
cozym <ddongyee99@daum.net>
2024-01-31 21:32:08 +0900

    feat: succeed 10859 javaVersion

--- src/Boj10859.java
+++ src/Boj10859.java
@@ -5,37 +5,47 @@ import java.util.StringTokenizer;
 
 // 뒤집어진 소수
 public class Boj10859 {
-    static String N;
+    static String N,res;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = st.nextToken();
+        res = "yes";
 
-        System.out.println(N);
+        if (!checkPrime(Long.parseLong(N)) || !checkPrime(reverseDigit(N))) {
+            res = "no";
+        }
+        System.out.println(res);
     }
 
     static long reverseDigit(String n) {
         StringBuilder r = new StringBuilder();
-        String same = "0258";
-        String move = "1";
-        String change = "";
 
         for (int i = n.length()-1; i >= 0; i--) {
             String c = String.valueOf(n.charAt(i));
             if ("347".contains(c)) {
+                res = "no";
                 return -1;
-            }
-            if (c.equals("6")) {
+            } else if (c.equals("6")) {
                 c = "9";
             } else if (c.equals("9")) {
                 c = "6";
             }
             r.append(c);
         }
+        return Long.parseLong(String.valueOf(r));
     }
 
-    static void checkPrime() {
-
+    static boolean checkPrime(long n) {
+        if (n==1) {
+            return false;
+        }
+        for (long i = 2; i*i <= n; i++) {
+            if (n%i==0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
